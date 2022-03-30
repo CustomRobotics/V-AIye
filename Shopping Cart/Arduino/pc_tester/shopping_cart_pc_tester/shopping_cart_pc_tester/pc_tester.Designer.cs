e5eb20eed1077a59e25221da23d1cb73edff555c
Custom Robotics <custom.robotics.gdl@gmail.com>
2022-03-29 19:43:23 -0600

    Adding: - PC Tester Application - 3D Model - Images - Videos

---
+++ Shopping Cart/Arduino/pc_tester/shopping_cart_pc_tester/shopping_cart_pc_tester/pc_tester.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace shopping_cart_pc_tester
+{
+    partial class pc_tester
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRigth = new System.Windows.Forms.Button();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.btnCenter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(101, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(101, 56);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(75, 52);
+            this.btnForward.TabIndex = 0;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(101, 172);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 52);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(101, 114);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(20, 114);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 52);
+            this.btnLeft.TabIndex = 0;
+            this.btnLeft.Text = "Left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRigth
+            // 
+            this.btnRigth.Location = new System.Drawing.Point(182, 114);
+            this.btnRigth.Name = "btnRigth";
+            this.btnRigth.Size = new System.Drawing.Size(75, 52);
+            this.btnRigth.TabIndex = 0;
+            this.btnRigth.Text = "Rigth";
+            this.btnRigth.UseVisualStyleBackColor = true;
+            this.btnRigth.Click += new System.EventHandler(this.btnRigth_Click);
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(12, 246);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResponse.Size = new System.Drawing.Size(260, 87);
+            this.txtResponse.TabIndex = 1;
+            // 
+            // btnCenter
+            // 
+            this.btnCenter.Location = new System.Drawing.Point(101, 143);
+            this.btnCenter.Name = "btnCenter";
+            this.btnCenter.Size = new System.Drawing.Size(75, 23);
+            this.btnCenter.TabIndex = 2;
+            this.btnCenter.Text = "Center";
+            this.btnCenter.UseVisualStyleBackColor = true;
+            this.btnCenter.Click += new System.EventHandler(this.btnCenter_Click);
+            // 
+            // pc_tester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 345);
+            this.Controls.Add(this.btnCenter);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnRigth);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "pc_tester";
+            this.Text = "Shopping Cart Tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRigth;
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Button btnCenter;
+    }
+}
+
---
+++ Shopping Cart/Arduino/pc_tester/shopping_cart_pc_tester/shopping_cart_pc_tester/pc_tester.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace shopping_cart_pc_tester
+{
+    public partial class pc_tester : Form
+    {
+        SerialPort serial;
+
+        public pc_tester()
+        {
+            InitializeComponent();
+
+            serial = new SerialPort();
+            serial.PortName = "COM4";
+            serial.BaudRate = 9600;
+            serial.DataReceived += Serial_DataReceived;
+        }
+
+        private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            this.Invoke(new Action(() => { txtResponse.AppendText(serial.ReadLine() + Environment.NewLine); }));
+        }
+
+        private void btnOpen_Click(object sender, System.EventArgs e)
+        {
+            if(btnOpen.Text=="Open")
+            {
+                btnOpen.Text = "Close";
+                serial.Open();
+            }
+            else
+            {
+                btnOpen.Text = "Open";
+                serial.Close();
+            }
+        }
+
+        private void btnForward_Click(object sender, System.EventArgs e)
+        {
+            serial.WriteLine("F");
+        }
+
+        private void btnBack_Click(object sender, System.EventArgs e)
+        {
+            serial.WriteLine("B");
+        }
+
+        private void btnLeft_Click(object sender, System.EventArgs e)
+        {
+            serial.WriteLine("L");
+        }
+
+        private void btnRigth_Click(object sender, System.EventArgs e)
+        {
+            serial.WriteLine("R");
+        }
+
+        private void btnStop_Click(object sender, System.EventArgs e)
+        {
+            serial.WriteLine("S");
+        }
+
+        private void btnCenter_Click(object sender, EventArgs e)
+        {
+            serial.WriteLine("C");
+        }
+    }
+}
